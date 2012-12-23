dd68ca19caa868ba4e00a3c6114b3e36c97cb89c
doktoric <doktoricsrichard@gmail.com>
2012-12-23 19:11:23 +0100

    database bugs

--- src/main/java/com/acme/doktorics/domain/Message.java
+++ src/main/java/com/acme/doktorics/domain/Message.java
@@ -2,6 +2,7 @@ package com.acme.doktorics.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,7 +12,7 @@ import javax.persistence.Table;
 public class Message {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String messageDate;
 	private String messageText;
